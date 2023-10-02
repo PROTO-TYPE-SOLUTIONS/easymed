150771c1c9554b1cc101d65ec9740ff661fbc685
MarcosDMT <marcosgav80@gmail.com>
2023-10-02 12:07:48 +0300

    added schedule and phamarcy pages

--- front-end/src/pages/dashboard/messages/index.js
+++ front-end/src/pages/dashboard/messages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import CustomizedLayout from '@/components/layout/customized-layout'
+import { Container } from '@mui/material'
 
 const Messages = () => {
   return (
-    <div>Messages</div>
+    <Container maxWidth="xl">Messages</Container>
   )
 }
 
+
+Messages.getLayout = (page) => (
+  <CustomizedLayout>{page}</CustomizedLayout>
+)
 export default Messages
