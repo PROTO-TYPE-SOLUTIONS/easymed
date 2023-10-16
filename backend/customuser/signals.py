39c53be90e3e8f6333ce224144a63b0f588df0d7
Jaydrey <jaribwetshi64@gmail.com>
2023-10-16 14:58:25 +0300

    feat(signals): create psot save user signal for user profile

---
+++ backend/customuser/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# models
+from .models import (
+    CustomUser,
+)
+from receptionists.models import ReceptionistProfile
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender: CustomUser, instance: CustomUser, created: bool, **kwargs):
+    if not created:
+        return
+    if instance.role == CustomUser.RECEPTIONIST:
+        try:
+            ReceptionistProfile.objects.create(user=instance)
+            return
+        except Exception as e:
+            return
+    
