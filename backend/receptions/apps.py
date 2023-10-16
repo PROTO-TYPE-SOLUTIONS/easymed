b32f466b4b6fd385b149ae321bfc054722715d30
Jaydrey <jaribwetshi64@gmail.com>
2023-10-16 17:04:50 +0300

    fix(apps file): change the receptions config name

--- backend/receptions/apps.py
+++ backend/receptions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReceptionConfig(AppConfig):
+class ReceptionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reception'
