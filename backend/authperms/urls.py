a67749d82d10b787ba435ee92fb9010f8fc3d832
Jaydrey <jaribwetshi64@gmail.com>
2023-10-16 12:50:35 +0300

    feat(urls): add group and perm endpoints for user and edit

--- backend/authperms/urls.py
+++ backend/authperms/urls.py
@@ -4,19 +4,35 @@ from django.urls import path
 from .views import (
     GroupsAPIView,
     AddGroupAPIView,
+    EditGroupNameAPIView,
+    DeleteGroupAPIView,
+    RemoveUserFromGroupAPIView,
     GroupAPIView,
+    UserGroupsAPIView,
     AddUserToGroupAPIView,
     PermissionsAPIView,
     PermissionAPIView,
     AddPermissionAPIView,
+    DeletePermissionAPIView,
+    AddPermissionsToUserAPIView,
+    EditPermissionAPIView,
+    UserPermissionsAPIView,
+    RemovePermissionsFromUserAPIView,
 )
 
 urlpatterns = [
+    # groups
     path("groups", GroupsAPIView.as_view(), name="groups"),
-    path("groups/<int:group_id>", GroupAPIView.as_view(), name="add-group"),
-    path("add-group", AddGroupAPIView.as_view(), name="add-group"),
-    path("add-user-to-group/<int:user_id>", AddUserToGroupAPIView.as_view(), name="add-user-to-group"),
+    path("groups/<int:group_id>", GroupAPIView.as_view(), name="group"),
+    path("groups/add", AddGroupAPIView.as_view(), name="add-group"),
+    path("groups/<str:group_name>/edit", EditGroupNameAPIView.as_view(), name="edit-group"),
+    path("groups/<str:group_name>/delete", DeleteGroupAPIView.as_view(), name="delete-group"),
+    path("groups/add-user/<str:user_id>", AddUserToGroupAPIView.as_view(), name="add-user-to-group"),
+    path("groups/user/<str:user_id>", UserGroupsAPIView.as_view(), name="user-groups"),
+    path("groups/remove-user/<str:user_id>", RemoveUserFromGroupAPIView.as_view(), name="remove-user-from-group"),
+    # permissions
     path("permissions", PermissionsAPIView.as_view(), name="permissions"),
     path("permissions/<int:permission_id>", PermissionAPIView.as_view(), name="permission"),
-    path("add-permission", AddPermissionAPIView.as_view(), name="add-permission")
+    path("permissions/add", AddPermissionAPIView.as_view(), name="add-permission"),
+    path("permissions/<int:permission_id>/delete", DeletePermissionAPIView.as_view(), name="delete-permission"),
 ]
