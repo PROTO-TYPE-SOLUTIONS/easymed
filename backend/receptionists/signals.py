c322fc0805ca91a6e51435b6ff91f307f214cd28
Jaydrey <jaribwetshi64@gmail.com>
2023-10-16 16:44:52 +0300

    fix(app): change receptionist to receptions

--- backend/receptionists/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-# models
-from .models import ReceptionistProfile
-from customuser.models import CustomUser
-
-# @receiver(post_save, sender=)
