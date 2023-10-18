58ef76f77aa37af46d151843b8adac1686b74664
Jaydrey <jaribwetshi64@gmail.com>
2023-10-18 13:54:59 +0300

    feat(permissions): create role permission classes

---
+++ backend/authperms/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework.permissions import BasePermission
+
+# models
+from customuser.models import (
+    CustomUser,
+    Doctor,
+    Nurse,
+    LabTech,
+    Patient,
+)
+
+class IsDoctorUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff and request.user.role==Doctor.BASE_ROLE)
+    
+class IsNurseUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff and request.user.role==Nurse.BASE_ROLE)
+    
+class IsLabTechUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff and request.user.role==LabTech.BASE_ROLE)
+    
+
+class IsSystemsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff and request.user.role==CustomUser.BASE_ROLE)
+    
+class IsPatientUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user  and request.user.role==Patient.BASE_ROLE)
+    
