17a53c4e68a2fd50da050ade8a2ae0ef1746b374
Moses Mbadi <57032428+mosesmbadi@users.noreply.github.com>
2023-10-19 20:31:54 +0300

    updated urls

--- backend/makeeasyhmis/urls.py
+++ backend/makeeasyhmis/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('patients/', include('patient.urls')),
     path('users/', include('customuser.urls')),
 
-    path('api/v1/lab/', include('laboratory.urls')), 
-    path('api/v1/inventory/', include('inventory.urls')), 
-    path("api/v1/authperms/", include("authperms.urls")),
+    path('lab/', include('laboratory.urls')), 
+    path('inventory/', include('inventory.urls')), 
+    path("authperms/", include("authperms.urls")),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),  
