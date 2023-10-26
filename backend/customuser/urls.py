f0b608f9a72f4f1773d22b634fd215c96d69a9c7
Jaydrey <jaribwetshi64@gmail.com>
2023-10-26 18:16:11 +0300

    feat(urls): add the doctor view in the url endpoints

--- backend/customuser/urls.py
+++ backend/customuser/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('doctors/', DoctorsAPIView.as_view(), name='doctors'),
     path('register/', RegistrationAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
 ]
