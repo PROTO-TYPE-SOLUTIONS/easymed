864a1b144b511b51b20aaccb75096b14e49f9227
Jaydrey <jaribwetshi64@gmail.com>
2023-10-31 10:15:49 +0300

    feat(serializers): add age property field to user serializer

--- backend/customuser/serializers.py
+++ backend/customuser/serializers.py
@@ -4,37 +4,50 @@ from .models import CustomUser, Doctor
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
+    age = serializers.SerializerMethodField()
     class Meta:
         model = CustomUser
-        fields = ('id', 'email', 'first_name', 'last_name', 'role')
+        fields = ('id', 'email', 'first_name',
+                  'last_name', 'role', 'profession', 'age')
+        
+    def get_age(self, obj: CustomUser):
+        if obj.age:
+            return obj.age
+        return None
+
 
 class CustomUserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password', 'first_name', 'last_name', 'role')
+        fields = ('email', 'password', 'first_name',
+                  'last_name', 'role', 'profession')
 
-    def create(self, validated_data:dict):
+    def create(self, validated_data: dict):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
             password=validated_data['password'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            role=validated_data['role']
+            role=validated_data['role'],
+            profession=validated_data['profession'],
         )
         return user
+
 
 class CustomUserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
-    def validate(self, data):
+    def validate(self, data: dict):
         email = data.get('email')
         password = data.get('password')
 
-        user = authenticate(request=self.context.get('request'), email=email, password=password)
+        user = authenticate(request=self.context.get(
+            'request'), email=email, password=password)
 
         if not user:
             raise serializers.ValidationError("Invalid email or password.")
@@ -43,9 +56,8 @@ class CustomUserLoginSerializer(serializers.Serializer):
             'user': user
         }
 
+
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = "__all__"
-
-        
