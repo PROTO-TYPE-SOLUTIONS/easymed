dfa48409f98d59c3f6c4124c759b7a851cadb54f
Jaydrey <jaribwetshi64@gmail.com>
2023-11-07 16:19:18 +0300

    feat(views): add permission classes to the viewset endpoints

--- backend/laboratory/views.py
+++ backend/laboratory/views.py
@@ -1,5 +1,14 @@
 from rest_framework import viewsets
-from .models import LabReagent, LabTestResult, LabTestRequest, LabTestCategory, LabTestProfile
+
+# models
+from .models import (
+    LabReagent, 
+    LabTestResult, 
+    LabTestRequest, 
+    LabTestCategory, 
+    LabTestProfile
+)
+# serializers
 from .serializers import (
     LabReagentSerializer,
     LabTestResultSerializer,
@@ -8,22 +17,32 @@ from .serializers import (
     LabTestProfileSerializer,
 )
 
+# permissions
+from authperms.permissions import (
+    IsStaffUser
+)
+
 class LabReagentViewSet(viewsets.ModelViewSet):
     queryset = LabReagent.objects.all()
     serializer_class = LabReagentSerializer
+    permission_classes = (IsStaffUser,)
 
 class LabTestProfileViewSet(viewsets.ModelViewSet):
     queryset = LabTestProfile.objects.all()
-    serializer_class = LabTestProfileSerializer    
+    serializer_class = LabTestProfileSerializer
+    permission_classes = (IsStaffUser,)
 
 class LabTestResultViewSet(viewsets.ModelViewSet):
     queryset = LabTestResult.objects.all()
     serializer_class = LabTestResultSerializer
+    permission_classes = (IsStaffUser,)
 
 class LabTestRequestViewSet(viewsets.ModelViewSet):
     queryset = LabTestRequest.objects.all()
     serializer_class = LabTestRequestSerializer
+    permission_classes = (IsStaffUser,)
 
 class LabTestCategoryViewSet(viewsets.ModelViewSet):
     queryset = LabTestCategory.objects.all()
     serializer_class = LabTestCategorySerializer
+    permission_classes = (IsStaffUser,)
