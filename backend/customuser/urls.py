d2928967349aaad7fe443076ae229b2bb645611b
Jaydrey <jaribwetshi64@gmail.com>
2023-11-09 12:19:37 +0300

    feat(urls): add the doctors, nurses and labtechs endpoints

--- backend/customuser/urls.py
+++ backend/customuser/urls.py
@@ -4,6 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('doctors/', DoctorsAPIView.as_view(), name='doctors'),
+    path('nurses/', NurseAPIView.as_view(), name='nurses'),
+    path('labtechs/', LabTechAPIView.as_view(), name='labtechs'),
+    path('receptionists', ReceptionistAPIView.as_view(), name='receptionists'),
     path('register/', RegistrationAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
 ]
+
