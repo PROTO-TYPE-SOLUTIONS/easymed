753da25866d60fd568dae2a68179ac813e9e694d
Jaydrey <jaribwetshi64@gmail.com>
2023-11-09 12:53:40 +0300

    fix: adjust the permissions for laboratory endpoint

--- backend/laboratory/views.py
+++ backend/laboratory/views.py
@@ -19,30 +19,34 @@ from .serializers import (
 
 # permissions
 from authperms.permissions import (
-    IsStaffUser
+    IsStaffUser,
+    IsDoctorUser,
+    IsLabTechUser,
+    IsNurseUser,
+    IsSystemsAdminUser
 )
 
 class LabReagentViewSet(viewsets.ModelViewSet):
     queryset = LabReagent.objects.all()
     serializer_class = LabReagentSerializer
-    permission_classes = (IsStaffUser,)
+    permission_classes = (IsDoctorUser | IsNurseUser | IsLabTechUser,)
 
 class LabTestProfileViewSet(viewsets.ModelViewSet):
     queryset = LabTestProfile.objects.all()
     serializer_class = LabTestProfileSerializer
-    permission_classes = (IsStaffUser,)
+    permission_classes = (IsDoctorUser | IsNurseUser | IsLabTechUser,)
 
 class LabTestResultViewSet(viewsets.ModelViewSet):
     queryset = LabTestResult.objects.all()
     serializer_class = LabTestResultSerializer
-    permission_classes = (IsStaffUser,)
+    permission_classes = (IsDoctorUser | IsNurseUser | IsLabTechUser,)
 
 class LabTestRequestViewSet(viewsets.ModelViewSet):
     queryset = LabTestRequest.objects.all()
     serializer_class = LabTestRequestSerializer
-    permission_classes = (IsStaffUser,)
+    permission_classes = (IsDoctorUser | IsNurseUser | IsLabTechUser,)
 
 class LabTestCategoryViewSet(viewsets.ModelViewSet):
     queryset = LabTestCategory.objects.all()
     serializer_class = LabTestCategorySerializer
-    permission_classes = (IsStaffUser,)
+    permission_classes = (IsDoctorUser | IsNurseUser | IsLabTechUser,)
