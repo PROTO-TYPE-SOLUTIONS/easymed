f3fd1bf43b89fab0aedf5fdd39217c8700d63414
Moses Mbadi <57032428+mosesmbadi@users.noreply.github.com>
2023-12-01 21:02:37 +0300

    updated readme on permissions

--- backend/makeeasyhmis/settings/development.py
+++ backend/makeeasyhmis/settings/development.py
@@ -10,21 +10,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    "default":{
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": config("POSTGRES_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     "default":{
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("POSTGRES_DB"),
+#         "USER": config("POSTGRES_USER"),
+#         "PASSWORD": config("POSTGRES_PASSWORD"),
+#         "HOST": config("POSTGRES_HOST"),
+#         "PORT": config("POSTGRES_PORT"),
+#     }
+# }
--- front-end/README.md
+++ front-end/README.md
@@ -13,19 +13,6 @@ running manually  = baseURL: "http://127.0.0.1:8000",
 
 ###### Include your base_url in the field above then use the above encryption key
 
-# Adding Permissions
-
-* Doctor Dashboard => CAN_ACCESS_DOCTOR_DASHBOARD
-* General Dashboard => CAN_ACCESS_GENERAL_DASHBOARD
-* Admin Dashboard => CAN_ACCESS_ADMIN_DASHBOARD
-* Reception Dashboard => CAN_ACCESS_RECEPTION_DASHBOARD
-* Nursing Dashboard => CAN_ACCESS_NURSING_DASHBOARD
-* Laboratory Dashboard => CAN_ACCESS_LABORATORY_DASHBOARD
-* Patients Dashboard => CAN_ACCESS_PATIENTS_DASHBOARD
-* AI ASSISTANT Dashboard => CAN_ACCESS_AI_ASSISTANT_DASHBOARD
-* Announcement Dashboard => CAN_ACCESS_ANNOUNCEMENT_DASHBOARD
-* Pharmacy Dashboard => CAN_ACCESS_PHARMACY_DASHBOARD
-* Inventory Dashboard => CAN_ACCESS_INVENTORY_DASHBOARD
 
 Lastly run the development server using either of the following commands:
 
