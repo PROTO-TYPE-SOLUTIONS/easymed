d4e7a76dc46cbf0732bcb890ad457b54d2c55e30
lev-web <lnjiru43@gmail.com>
2024-01-11 01:17:23 +0300

    :hammer: updated font sizes

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -16,7 +16,19 @@ module.exports = {
       cardSecondary: '#0E4F82',
       gray: '#CCCCCC',
       link: '#26A0F8'
+    },
+    fontSize: {
+      xxs: '0.6rem',
+      xs: '0.75rem',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+    '2xl': '1.563rem',
+    '3xl': '1.953rem',
+    '4xl': '2.441rem',
+    '5xl': '3.052rem',
     }
+
   },
   plugins: [],
 }
