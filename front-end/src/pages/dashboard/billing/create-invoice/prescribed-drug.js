3fafa665d1d5412c6dc6f3666735a16c865a37a9
lev-web <lnjiru43@gmail.com>
2024-01-17 16:23:42 +0300

    :construction: :package: billing module

---
+++ front-end/src/pages/dashboard/billing/create-invoice/prescribed-drug.js
@@ -0,0 +1,53 @@
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { Container, Grid, Checkbox, FormControlLabel } from '@mui/material';
+
+import { getAllPatientBillingPrescribedDrug } from '@/redux/features/billing';
+
+const PrescribedDrug = ({patient_id, setSelectedPrescribedDrugs}) => {
+    const dispatch = useDispatch()
+    const { patientPrescribedDrug } =  useSelector((store) => store.billing);
+
+    const handleCheckboxChange = (event, drug) => {
+        const isChecked = event.target.checked;
+    
+        if (isChecked) {
+          // If checkbox is checked, add the drug to the list of checked items
+          setSelectedPrescribedDrugs(prevItems => [...prevItems, drug]);
+        } else {
+          // If checkbox is unchecked, remove the drug from the list of checked items
+          setSelectedPrescribedDrugs(prevItems =>
+            prevItems.filter(item => item.id !== drug.id)
+          );
+        }
+    };
+
+    useEffect(()=>{
+        dispatch(getAllPatientBillingPrescribedDrug(patient_id))
+    }, [patient_id])
+  return (
+    <Container className='my-4' maxWidth="xl">
+        <Grid item md={12} xs={4}>
+            <h2 className='text-xl px-4 rounded-lg text-primary'>Prescribed Drugs</h2>
+        </Grid>
+        <ul>
+        {patientPrescribedDrug.length > 0 ? patientPrescribedDrug.map((drug, index) => {
+           return (            
+                <li key={`patientPrescribedDrug-${index}`}>
+                    <div className='flex items-center'>
+                        <FormControlLabel 
+                            control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 16 } }}/>} 
+                            label={drug.item_name} 
+                            onChange={(event) => handleCheckboxChange(event, drug)}
+                        />
+                    </div>
+                </li>           
+
+           )}) : <p className='text-warning px-4'> no Prescribed Drug found </p>
+        }
+        </ul>
+    </Container>
+  )
+}
+
+export default PrescribedDrug
--- front-end/src/pages/dashboard/billing/index.js
+++ front-end/src/pages/dashboard/billing/index.js
@@ -3,35 +3,16 @@ import CustomizedLayout from "../../../components/layout/customized-layout";
 import DashboardLayout from "@/components/layout/dashboard-layout";
 import { Container } from '@mui/material';
 import BilledDataGrid from './billed-datagrid';
-import AddInvoiceModal from '@/components/dashboard/billing/add-invoice-modal';
-import OverdueInvoices from '@/components/dashboard/billing/overdue-invoices';
-import BillingReports from '@/components/dashboard/billing/billing-reports';
-import InvoiceItems from '@/components/dashboard/billing/invoice-items';
+
+import BillingNav from '@/components/dashboard/billing/BillingNav';
 
 
 const Billing = () => {
-  const [currentTab,setCurrentTab] = useState(0)
 
   return (
     <Container maxWidth="xl" className='my-8'>
-      <div className="flex items-center gap-4 my-8">
-        <AddInvoiceModal />
-        <button onClick={()=> setCurrentTab(2)} className={`${currentTab === 2 ? 'bg-primary text-white' : 'bg-white shadow'}  text-sm rounded px-3 py-2 mb-1`}>
-          Overdue Invoices
-        </button>
-        <button onClick={()=> setCurrentTab(3)} className={`${currentTab === 3 ? 'bg-primary text-white' : 'bg-white shadow'}  text-sm rounded px-3 py-2 mb-1`}>
-          View Items
-        </button>
-        <button onClick={()=> setCurrentTab(4)} className={`${currentTab === 4 ? 'bg-primary text-white' : 'bg-white shadow'}  text-sm rounded px-3 py-2 mb-1`}>
-          Reports
-        </button>
-      </div>
-
-      {currentTab === 0 && <BilledDataGrid /> }
-      {/* {currentTab === 1 && <OverdueInvoices /> } */}
-      {currentTab === 2 && <OverdueInvoices /> }
-      {currentTab === 3 && <InvoiceItems /> }
-      {currentTab === 4 && <BillingReports /> }
+      <BillingNav/>
+      <BilledDataGrid />
     </Container>
   )
 }
