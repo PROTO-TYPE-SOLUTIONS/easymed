cfe20fcf91dc7c1b43d7874a7061b7a61af527f8
Moses Mbadi <mosesmbadi@gmail.com>
2024-02-03 20:34:06 +0300

    labtest endpoint updates

--- backend/billing/models.py
+++ backend/billing/models.py
@@ -7,15 +7,17 @@ from django.db.models import Sum
 def invoice_file_path(instance, filename):
     return f'invoices/{instance.invoice_number}/{filename}'
 
+
+
 class Invoice(models.Model):
     STATUS_CHOICES = (
         ('pending', 'Pending'),
         ('paid', 'Paid'),
     )
-    patient = models.ForeignKey(Patient, on_delete = models.SET_NULL, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
     invoice_number = models.CharField(max_length=50)
     invoice_date = models.DateField()
-    invoice_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    invoice_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='pending')
     invoice_description = models.CharField(max_length=200)
@@ -25,21 +27,61 @@ class Invoice(models.Model):
 
     def calculate_invoice_amount(self):
         # Sum up the total sale price of all related invoice items
-        total_amount = self.invoiceitem_set.aggregate(
+        total_amount = self.invoice_items.aggregate(
             total_amount=Sum('service__inventory__sale_price'))['total_amount'] or 0
         self.invoice_amount = total_amount
         self.save()
 
+    def save(self, *args, **kwargs):
+        # Calculate invoice amount before saving the instance
+        self.calculate_invoice_amount()
+        super().save(*args, **kwargs)
 
 
 class InvoiceItem(models.Model):
-    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='invoice_items')
     service = models.ForeignKey(Item, on_delete=models.CASCADE)
     item_created_at = models.DateTimeField(auto_now_add=True)
     item_updated_at = models.DateTimeField(auto_now=True)
-    # quantity = models.CharField(max_length=50, null=True)  
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         # Recalculate invoice amount after saving the InvoiceItem
         self.invoice.calculate_invoice_amount()
+        
+
+# class Invoice(models.Model):
+#     STATUS_CHOICES = (
+#         ('pending', 'Pending'),
+#         ('paid', 'Paid'),
+#     )
+#     patient = models.ForeignKey(Patient, on_delete = models.SET_NULL, null=True)
+#     invoice_number = models.CharField(max_length=50)
+#     invoice_date = models.DateField()
+#     invoice_amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     status = models.CharField(
+#         max_length=10, choices=STATUS_CHOICES, default='pending')
+#     invoice_description = models.CharField(max_length=200)
+#     invoice_file = models.FileField(upload_to=invoice_file_path, null=True, blank=True)
+#     invoice_created_at = models.DateTimeField(auto_now_add=True)
+#     invoice_updated_at = models.DateTimeField(auto_now=True)
+
+#     def calculate_invoice_amount(self):
+#         # Sum up the total sale price of all related invoice items
+#         total_amount = self.invoiceitem_set.aggregate(
+#             total_amount=Sum('service__inventory__sale_price'))['total_amount'] or 0
+#         self.invoice_amount = total_amount
+#         self.save()
+
+
+
+# class InvoiceItem(models.Model):
+#     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE,)
+#     service = models.ForeignKey(Item, on_delete=models.CASCADE)
+#     item_created_at = models.DateTimeField(auto_now_add=True)
+#     item_updated_at = models.DateTimeField(auto_now=True)
+
+#     def save(self, *args, **kwargs):
+#         super().save(*args, **kwargs)
+#         # Recalculate invoice amount after saving the InvoiceItem
+#         self.invoice.calculate_invoice_amount()
