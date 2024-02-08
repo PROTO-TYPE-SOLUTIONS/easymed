8178b9228443cf9bd0f4b742e1691261b3dce60f
lev-web <lnjiru43@gmail.com>
2024-02-08 14:02:58 +0300

    :lipstick: profile personal information interface

--- front-end/src/components/account/AccountProfile.js
+++ front-end/src/components/account/AccountProfile.js
@@ -50,29 +50,29 @@ const AccountProfile = () => {
       <div className='w-full border border-gray py-4 my-2 rounded shadow-lg'>
         <h5 className='mb-4 pl-4 font-bold'>Personal Information</h5>
         <Grid className='justify-center flex px-8' container spacing={2}>
-          <Grid className='my-4 gap-2 flex flex-col' xs={4}>
+          <Grid className='my-4' xs={4}>
             <label>First Name</label>
-            <p className='text-sm text-primary'>{`${currentUser.first_name}`}</p>
+            <p className='text-sm text-primary my-2'>{`${currentUser.first_name}`}</p>
           </Grid>
-          <Grid className='my-4 gap-2 flex flex-col' xs={4}>
+          <Grid className='my-4' xs={4}>
             <label>Last Name</label>
-            <p className='text-sm text-primary'>{`${currentUser.last_name}`}</p>          
+            <p className='text-sm text-primary my-2'>{`${currentUser.last_name}`}</p>          
           </Grid>
-          <Grid className='my-4 gap-2 flex flex-col' xs={4}>
+          <Grid className='my-4' xs={4}>
             <label>Email Address</label>
-            <p className='text-sm text-primary'>{`${currentUser.email}`}</p>
+            <p className='text-sm text-primary my-2'>{`${currentUser.email}`}</p>
           </Grid>
-          <Grid className='my-4 gap-2 flex flex-col' xs={4}>
+          <Grid className='my-4' xs={4}>
             <label>Phone Number</label>
-            <p className='text-sm text-primary'>{`${currentUser.phone}`}</p>          
+            <p className='text-sm text-primary my-2'>{`${currentUser.phone}`}</p>          
           </Grid>
-          <Grid className='my-4 gap-2 flex flex-col' xs={4}>
+          <Grid className='my-4' xs={4}>
             <label>Role</label>
-            <p className='text-sm text-primary'>{`${currentUser.role}`}</p>          
+            <p className='text-sm text-primary my-2'>{`${currentUser.role}`}</p>          
           </Grid>
-          <Grid className='my-4 gap-2 flex flex-col' xs={4}>
+          <Grid className='my-4' xs={4}>
             <label>Proffession</label>
-            <p className='text-sm text-primary'>{`${currentUser.profession}`}</p>          
+            <p className='text-sm text-primary my-2'>{`${currentUser.profession}`}</p>          
           </Grid>
         </Grid>        
       </div>
