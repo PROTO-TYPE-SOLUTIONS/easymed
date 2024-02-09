2e3473bc826be42e4e84926eae9a7038cdf66ee6
lev-web <lnjiru43@gmail.com>
2024-02-09 22:06:35 +0300

    :sparkles: added version modal and support modal

---
+++ front-end/src/components/layout/sidebar/VersionModal.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import { BsFillExclamationCircleFill } from "react-icons/bs";
+
+const VersionModal = () => {
+    const [open, setOpen] = useState(false);
+  
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  return (
+    <section>
+        <div onClick={handleClickOpen} className="flex items-center gap-2 cursor-pointer">
+            <BsFillExclamationCircleFill className="" />
+            <p>Make - Easy HMIS v1.0</p>
+        </div>
+        <Dialog
+            fullWidth
+            maxWidth="sm"
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogContent>
+                <div className="py-4">
+                    <h2 className="w-full text-center my-2 text-xl"> Make-Easy-HMIS - 2.0 </h2>
+                    <p className="w-full text-center my-2 text-xl">29ᵗʰ February, 2024</p>
+                </div>
+            </DialogContent>
+        </Dialog>
+    </section>
+  )
+}
+
+export default VersionModal
