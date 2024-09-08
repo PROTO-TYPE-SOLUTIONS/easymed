78b990bb0bccacbb9ff3eaa11f3778f8314c1486
levy <lnjiru43@gmail.com>
2024-09-08 21:05:20 +0300

    removed mpesa paymode

--- front-end/src/components/dashboard/billing/payOverview/PayAmountsDisplay.js
+++ front-end/src/components/dashboard/billing/payOverview/PayAmountsDisplay.js
@@ -3,9 +3,6 @@ import MpesaPayModal from './MpesaPayModal'
 
 const PayAmountsDisplay = (
     {    
-        appointmentMpesaSum,
-        prescribedDrugsMpesaSum,
-        labReqMpesaSum,
         appointmentCashSum,
         prescribedDrugsCashSum,
         labReqCashSum,
@@ -15,22 +12,12 @@ const PayAmountsDisplay = (
         appointmentSum,
         prescribedDrugsSum,
         labReqSum,
-        patient_id,
     }
 
     ) => {
   return (
     <div className='space-y-2 w-full'>
 
-            <div className='flex justify-end'>
-                <div className='border-b border-[#D3D3D3] w-48 justify-between flex items-center'>
-                    <h2 className='w-full '>Mpesa</h2>
-                    <h2 className='w-24 '>{appointmentMpesaSum + prescribedDrugsMpesaSum + labReqMpesaSum}</h2>
-                    {(appointmentMpesaSum + prescribedDrugsMpesaSum + labReqMpesaSum) > 0 && (
-                        <MpesaPayModal patient_id={patient_id} amount={(appointmentMpesaSum + prescribedDrugsMpesaSum + labReqMpesaSum)}/>
-                    )}
-                </div>
-            </div>
             <div className='flex justify-end'>
                 <div className='border-b border-[#D3D3D3] w-48 justify-between flex'>
                     <h2 className='border-b border-[#D3D3D3] w-full '>Cash</h2>
