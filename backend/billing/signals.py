5569ea7d1884e5437cebc9109e2318b74c15ca6b
Moses Mbadi <mosesmbadi@gmail.com>
2025-01-08 22:34:19 +0300

    cleaned up celery tasks

--- backend/billing/signals.py
+++ backend/billing/signals.py
@@ -5,35 +5,7 @@ from django.core.exceptions import ValidationError
 
 from .utils import check_quantity_availability, update_service_billed_status
 from inventory.models import Inventory
-from .models import Invoice, InvoiceItem
-from easymed.celery_tasks import (
-    send_invoice_created_email,
-    send_invoice_updated_email,
-)
-
-
-# '''signal to fire up celery task to  to generated pdf once Invoice tale gets a new entry'''
-# @receiver(post_save, sender=Invoice)
-# def generate_invoice(sender, instance, created, **kwargs):
-#     if created:
-#         generate_invoice_pdf.delay(instance.pk,)
-
-
-''''
-Let's send an email whenever an invoice is created or status field changes
-'''
-@receiver(post_save, sender=Invoice)
-def handle_invoice_created(sender, instance, created, **kwargs):
-    if created:
-        send_invoice_created_email.delay(instance.id)
-
-
-def handle_invoice_status_change(sender, instance, created, **kwargs):
-    if not created:
-        if instance.status != instance.status:
-            send_invoice_updated_email.delay(instance.id)
-    instance._previous_status = instance.status
-
+from .models import InvoiceItem
 
 
 @receiver(post_save, sender=InvoiceItem)
