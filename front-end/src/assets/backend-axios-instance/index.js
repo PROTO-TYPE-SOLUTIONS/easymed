b28cfa6492e596407501af127c55de79f3f62296
clairemuiru1 <muiruclaire@gmail.com>
2025-02-05 13:40:01 +0300

    "reinstalling the project"

--- front-end/src/assets/backend-axios-instance/index.js
+++ front-end/src/assets/backend-axios-instance/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const backendAxiosInstance = axios.create({
-    baseURL: "http://api:8080/",
+    // baseURL: "http://api:8080/",
+    baseURL: "http://backend:8000"
 });
 
 backendAxiosInstance.interceptors.request.use(async (request) => {
-    console.log("REQUEST ",request);
+    console.log("REQUEST ", request);
     return request;
 });
